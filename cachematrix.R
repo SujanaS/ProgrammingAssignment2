a7b3a91dc6a6e33307763e640bedbceb5599e8cd
Sujana Sarath <sujana.m1005@gmail.com>
2017-04-23 08:55:21 +0530

    cachematrix.R uploaded
    Coursera Assignment 2 - cachematrix inversing

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is for Coursera Programming assignment 2 - getting cached matrix in inversible
+## format
 
-## Write a short comment describing this function
+## Below function gets the cached matrix that is passed
 
 makeCacheMatrix <- function(x = matrix()) {
+  m<- NULL
+  set <- function(y){
+    x<<- y
+    m <<- NULL
+  }
+  get <- function()x
+  setinverse <- function(solve) m<<-solve
+  getinverse <- function()m
+  list(set = set, get = get,setinverse = setinverse, getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## Below function is to inverse the cached matix using solve function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)){
+    message("getting cached matrix")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data,...)
+  x$setinverse(m)
+  m
 }
